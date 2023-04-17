06df0865dc005b195dbda9b04afe7ab14fd6beb3
Todor Todorov <78209454+TodorTodoroff@users.noreply.github.com>
2023-04-17 08:45:17 +0300

    add new tasks

---
+++ Infrastructure/M1/m1/p2/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+    }
+  }
+}
+resource "docker_image" "img-web" {
+  name = "shekeriev/terraform-docker:latest"
+}
+
+resource "docker_container" "con-web" {
+  name  = "site"
+  image = docker_image.img-web.image_id
+  ports {
+    internal = "80"
+    external = "80"
+  }
+}
+
+provider "docker" {
+  host = "tcp://192.168.99.100:2375/"
+}
